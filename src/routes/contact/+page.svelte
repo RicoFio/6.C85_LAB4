20711cb615105d641bfc88184319486172020419
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2024-03-05 12:10:29 -0500

    added missing contacts form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,3 +6,14 @@
 <svelte:head>
 	<title>Contact me</title>
 </svelte:head>
+
+<h1>Contact me!</h1>
+<form action="mailto:fiorista@mit.edu" method="POST" enctype="text/plain">
+    <label for="email">Email:</label>
+    <input type="email">
+    <label for="text">Subject:</label>
+    <input type="text">
+    <label for="">Message:</label>
+    <textarea name="body"></textarea>
+    <button>Submit</button>
+</form>
