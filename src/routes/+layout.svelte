90e9adef5a59e02954fd15c3b41cac4a11b65ddf
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2024-03-13 02:09:23 -0400

    finished lab 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,14 @@
     {url: "https://github.com/ricofio", title: "GitHub"},
     ];
     import { page } from '$app/stores';
+    let colorScheme = "light dark";
+    let root = globalThis?.document?.documentElement;
+    root?.style.setProperty("color-scheme", colorScheme);
+    $: root?.style.setProperty("color-scheme", colorScheme);
+    let localStorage = globalThis.localStorage ?? {};
+    localStorage.colorScheme = colorScheme;
 </script>
+
 
 <nav>
 	{#each pages as p }
@@ -16,5 +23,42 @@
         </a>
 	{/each}
 </nav>
+
+<style>
+    nav a{
+        flex: 1;
+        text-decoration: none;
+        color: inherit;
+        text-align: center;
+        padding: 0.5em;
+    }
+
+    nav {
+        --border-color: oklch(50% 10% 200 / 40%);
+        display: flex;
+        margin-bottom: 3em;
+        border-bottom-width: 1px;
+        border-bottom-style: solid;
+        /* border-bottom-color: oklch(80% 3% 200); */
+        /* border-bottom-color: oklch(50% 10% 200); */
+        border-bottom-color: var(--border-color);
+    }
+
+    .color-scheme {
+        position: absolute;
+        top: 1rem;
+        right: 2rem;
+        font: 80%/1.5 system-ui;
+
+    }
+
+    select {
+        font: inherit;
+        display: block;
+        width: 160%;
+        box-sizing: border-box;
+        margin-block: 0.3em;
+    }
+</style>
 
 <slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,33 @@
 <p>Please see the below kittens to lighten up your day:</p>
 <img src="images/kittens.webp" alt="Kittens"/>
 <br/>
+
+{#await fetch("https://api.github.com/users/ricofio") }
+    <p>Loading...</p>
+{:then response}
+    {#await response.json()}
+        <p>Decoding...</p>
+    {:then data}
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{ JSON.stringify(data.followers) }</dd>
+            <dt>Following</dt>
+            <dd>{ JSON.stringify(data.following) }</dd>
+            <dt>Nr. of Public Repositories</dt>
+            <dd>{ JSON.stringify(data.public_repos) }</dd>
+        </dl>
+    {:catch error}
+        <p class="error">
+            Something went wrong: {error.message}
+        </p>
+    {/await}
+{:catch error}
+    <p class="error">
+        Something went wrong: {error.message}
+    </p>
+{/await}
+<br/>
 <p>
     Feel free to contact me if you have any questions or if you would like to 
     work together. The best way to reach me is either through the contact form or 
